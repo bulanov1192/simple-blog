8526fad0bf9fd9c010dbb2c5926adf0de27936c0
bulanov1192 <bulanov1192@gmail.com>
2024-09-08 03:22:57 +0500

    Linter config turned off

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config, { defaultLoaders }) => {
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
     return config;
